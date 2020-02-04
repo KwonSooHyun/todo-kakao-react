b990f9eb90783860ddc15ae02fc6433e65b314af
EooHoSun <redysj0517@naver.com>
2020-02-04 22:30:42 +0900

    make App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
 import './App.scss';
 import TodoList from './components/TodoList';
-
-
-
+interface Todo{
+    "id" : string;
+    "recordDate" : string;
+    "completed" : boolean;
+    "text" : string;
+}
 type AppProps = {
     text : string;
-    todos : Object;
+    todoList : Array<Todo>;
     modalState : string;
 }
 
-export default function App({text , todos, modalState} : AppProps) {
+export default function App({text , todoList, modalState} : AppProps) {
 
     init();
 
@@ -19,16 +22,8 @@ export default function App({text , todos, modalState} : AppProps) {
                 localstorage의 todos / 빈객체 전달
     */
     function init() : void{
-        todos = {
-            "1" : {
-                "text" : "",
-                "recordDate" : "",
-                "completed" : false
-
-            }
-        }}
-        const encodedTodos : string | null = localStorage ? localStorage.getItem("todos") : "{}";
-        todos = isTodosExist_(localStorage) ? decodeTodos_(encodedTodos) : JSON.parse("{}");
+        const encodedTodos : any = localStorage ? localStorage.getItem("todoList") : [];
+        todoList = isTodosExist_(localStorage) ? decodeTodos_(encodedTodos) : [];
         modalState = "none"
     }
 
@@ -37,25 +32,25 @@ export default function App({text , todos, modalState} : AppProps) {
         기능 : localStorage의 todos 존재 여부 check
     */
     function isTodosExist_(storage : Storage) : boolean{
-        return !!storage.getItem("todos");
+        return !!storage.getItem("todoList");
     }
 
      //암호화
-     function encodeTodos_(decodedTodos : string | null) : string{
-        const encodedTodos= escape(decodedTodos as string);
-        return encodedTodos;
+     function encodeTodos_(decodedTodos : Array<any>) : Array<Todo>{
+        decodedTodos.forEach(element => element.text = escape(element.text));
+        return decodedTodos;
     }   
 
     //복호화
-    function decodeTodos_(encodedTodos : string | null) : string{
-        const decodedTodos= unescape(encodedTodos as string);
-        return decodedTodos;
+    function decodeTodos_(encodedTodos : Array<any>) : Array<Todo>{
+        encodedTodos.forEach(element => element.text = escape(element.text));
+        return encodedTodos;
     }
 
     /*
         기능 : 난수 생성 (무한loop방지를 위해 max 10설정)
     */
-    function makeId_(todoIds : Array<string>) : string{
+    function makeId_(todos : Array<Todo>) : string{
         let maxLoop : number = 10;
         let ranNum : string = "";
 
@@ -64,7 +59,7 @@ export default function App({text , todos, modalState} : AppProps) {
             ranNum = (()=>{
                 return Math.floor(1+Math.random()*0x10000).toString().substring(1);
             })();
-            if(todoIds.indexOf(ranNum) == -1){
+            if(todoList.filter(element=>element.id == ranNum).length > 0){
                 return ranNum;
             }
         }
@@ -73,25 +68,23 @@ export default function App({text , todos, modalState} : AppProps) {
 
 
     const addHandler = {
-        addTodo : function(id : string, text : string, todos : Object) : void{
-            if(Object.keys(todos).indexOf(id) > -1 ){
+        addTodo : function(id : string, text : string) : void{
+            if(Object.keys(todoList).indexOf(id) > -1 ){
                 //this.showModal("이미 존재하는 일정입니다.");
                 return;
             }
-            const todo = {
+            const now = new Date();
+            const todo : Todo = {
+                "id" : id,
                 "text" : text,
-                "recordDate" : (new Date()).toLocaleString,
+                "recordDate" : (new Date()).toLocaleString(),
                 "completed" : false
             }
-            todos[id] = todo;
+            todoList.push(todo);
         },
 
-        deleteTodo : function(id : string, todos : Object){
-            if(Object.keys(todos).indexOf(id) == -1 ){
-                //this.showModal("존재하지않는 일정입니다.");
-                return;
-            }
-            delete
+        deleteTodo : function(id : string){
+            todoList = todoList.filter(element => element.id != id);
         },
 
         changeTodoState : function(id : string) : void{
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 export default function TodoList(props : string){
     console.log(props);
     return(
